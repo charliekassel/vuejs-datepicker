dd693006be70bd8ac7b999b6e17831790c31222d
Charlie Kassel <charlie.kassel@vice.com>
2016-10-11 10:03:19 +0100

    formatting changes

--- src/utils/DateLanguages.js
+++ src/utils/DateLanguages.js
@@ -25,13 +25,13 @@ export default {
     },
 
     'nl': {
-      'months' : {
+      'months': {
         'original': ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
         'abbr': ['Jan', 'Feb', 'Maa', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
       },
       'days': ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za']
     },
-    
+
     'pt-br': {
       'months': {
         'original': ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
