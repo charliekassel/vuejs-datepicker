99464c8e73edd7b9915369de2d713b5fd837eed0
Kyul <kyulmail@gmail.com>
2017-03-27 17:19:27 +0900

    Fix missing brace

--- src/utils/DateLanguages.js
+++ src/utils/DateLanguages.js
@@ -192,7 +192,8 @@ export default {
         'abbr': ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월']
       },
       'days': ['일', '월', '화', '수', '목', '금', '토']
-
+    },
+    
     'nb-no': {
       'months': {
         'original': ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
