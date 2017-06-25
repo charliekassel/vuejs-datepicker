79fcd1dad4cdd8d424f740dcc04924d527a68a6c
Charlie Kassel <charlie.kassel@vice.com>
2017-06-25 13:07:07 +0100

    exclude demo files form coverage

--- test/unit/index.js
+++ test/unit/index.js
@@ -9,5 +9,8 @@ testsContext.keys().forEach(testsContext)
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const srcContext = require.context('../../src', true, /^\.\/(?!main(\.js)?$)/)
+const srcContext = require.context('../../src', true, /^\.\/(?!main|App(\.js|\.vue)?$)/)
+
+console.log(srcContext.keys())
+
 srcContext.keys().forEach(srcContext)
