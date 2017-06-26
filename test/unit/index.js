302f1ea9d6a0df3fd254dc7065aca8ff06dbb15a
Charlie Kassel <charlie.kassel@vice.com>
2017-06-26 08:18:02 +0100

    rename App component to Demo

--- test/unit/index.js
+++ test/unit/index.js
@@ -9,7 +9,7 @@ testsContext.keys().forEach(testsContext)
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const srcContext = require.context('../../src', true, /^\.\/(?!main|App(\.js|\.vue)?$)/)
+const srcContext = require.context('../../src', true, /^\.\/(?!main|Demo(\.js|\.vue)?$)/)
 
 console.log(srcContext.keys())
 
