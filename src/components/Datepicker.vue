09cb9a2e72b0b08ceedb54f1811c5d34ec9862cd
Kelly Milligan <kelly@milligan.net.nz>
2017-07-12 17:01:46 +0100

    Add a flag to show the full month name in head

--- src/components/Datepicker.vue
+++ src/components/Datepicker.vue
@@ -103,6 +103,10 @@ export default {
       value: String,
       default: 'en'
     },
+    fullMonthName: {
+      value: Boolean,
+      default: false
+    },
     disabled: {
       type: Object
     },
@@ -204,7 +208,7 @@ export default {
     },
     currMonthName () {
       const d = new Date(this.pageDate)
-      return DateUtils.getMonthNameAbbr(d.getMonth(), this.translation.months.abbr)
+      return DateUtils.getMonthNameAbbr(d.getMonth(), this.fullMonthName ? this.translation.months.original : this.translation.months.abbr)
     },
     currYear () {
       const d = new Date(this.pageDate)
