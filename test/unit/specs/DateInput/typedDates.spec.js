f357eb1eacef6d7eea6ff00c6770dc510a006774
Charlie Kassel <charlie.kassel@vice.com>
2018-05-03 19:45:34 +0100

    add tests for return keyup

--- test/unit/specs/DateInput/typedDates.spec.js
+++ test/unit/specs/DateInput/typedDates.spec.js
@@ -29,12 +29,18 @@ describe('DateInput', () => {
     expect(wrapper.vm.formattedValue).toEqual(dateString)
   })
 
-  it('emit the date when typed', () => {
+  it('emits the date when typed', () => {
     const input = wrapper.find('input')
     wrapper.vm.input.value = '2018-04-24'
-    input.trigger('keypress')
+    input.trigger('keyup')
     expect(wrapper.emitted().typedDate).toBeDefined()
     expect(wrapper.emitted().typedDate[0][0]).toBeInstanceOf(Date)
+  })
+
+  it('emits closeCalendar when return is pressed', () => {
+    const input = wrapper.find('input')
+    input.trigger('keyup', {keyCode: 13})
+    expect(wrapper.emitted().closeCalendar).toBeDefined()
   })
 
   it('returns when blurred if date has not been typed', () => {
@@ -47,18 +53,18 @@ describe('DateInput', () => {
     input.trigger('blur')
     expect(wrapper.emitted().clearDate).toBeDefined()
   })
-})
 
-it('doesn\'t emit the date if typeable=false', () => {
-  const wrapper = shallow(DateInput, {
-    propsData: {
-      format: 'dd MMM yyyy',
-      translation: en,
-      typeable: false
-    }
+  it('doesn\'t emit the date if typeable=false', () => {
+    const wrapper = shallow(DateInput, {
+      propsData: {
+        format: 'dd MMM yyyy',
+        translation: en,
+        typeable: false
+      }
+    })
+    const input = wrapper.find('input')
+    wrapper.vm.input.value = '2018-04-24'
+    input.trigger('keyup')
+    expect(wrapper.emitted().typedDate).not.toBeDefined()
   })
-  const input = wrapper.find('input')
-  wrapper.vm.input.value = '2018-04-24'
-  input.trigger('keypress')
-  expect(wrapper.emitted().typedDate).not.toBeDefined()
 })
