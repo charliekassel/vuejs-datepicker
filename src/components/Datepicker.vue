c774d5bb046733384955d36c083abc9657ff0e05
gaaab <kisuna.ga@gmail.com>
2019-03-14 10:29:31 +0800

    Update Datepicker

--- src/components/Datepicker.vue
+++ src/components/Datepicker.vue
@@ -258,6 +258,7 @@ export default {
         return this.close(true)
       }
       this.setInitialView()
+      this.$emit('opened')
     },
     /**
      * Sets the initial picker page view: day, month or year
