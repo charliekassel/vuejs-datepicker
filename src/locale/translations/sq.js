3f3b3bf7854d0c56c2d2723831cde9e829b030ab
Kristi Jorgji <kristi.jorgji@INR00009.local>
2019-11-28 16:08:50 +0100

    add sq locale

---
+++ src/locale/translations/sq.js
@@ -0,0 +1,10 @@
+import Language from '../Language'
+
+export default new Language(
+  'Albanian',
+  ['Janar', 'Shkurt', 'Mars', 'Prill', 'Maj', 'Qershor', 'Korrik', 'Gusht', 'Shtator', 'Tetor', 'Nëntor', 'Dhjetor'],
+  ['Jan', 'Shku', 'Mar', 'Pri', 'Maj', 'Qer', 'Korr', 'Gush', 'Shta', 'Tet', 'Nën', 'Dhje'],
+  ['Djelë', 'Hënë', 'Martë', 'Mërkurë', 'Enjte', 'Premte', 'Shtunë']
+)
+// eslint-disable-next-line
+;
