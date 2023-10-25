1799a95cd627f4876ac02759bd1dc3228312b6ac
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2023-10-25 16:08:24 -0700

    feat: update package.exports so locales and style.css is accessible

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import {resolve} from 'path'
+import fs from 'fs';
+import path from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const locales = fs.readdirSync('./src/locale/translations').map((locale) => ([
+  path.join('locale/translations', path.basename(locale, '.js')),
+  path.resolve(__dirname, 'src/locale/translations', locale),
+]))
 
 export default defineConfig({
   plugins: [
@@ -8,7 +14,11 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.js'),
+      entry: {
+        'vuejs-datepicker': path.resolve(__dirname, 'src/main.js'),
+        ...Object.fromEntries(locales),
+        'locale/index': path.resolve(__dirname, 'src/locale/index.js'),
+      },
       name: '@tunezilla/vuejs-datepicker',
       formats: ['es', 'cjs'],
     },
